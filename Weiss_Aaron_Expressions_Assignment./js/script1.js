cf3d26af41b52a894b2f09f546e95ce5750f2e51
AAR0NWEISS <aoweiss@fullsail.edu>
2015-05-14 22:25:15 -0400

    set arry strings
    set array strings need to remove markers and set var holders

--- Weiss_Aaron_Expressions_Assignment./js/script1.js
+++ Weiss_Aaron_Expressions_Assignment./js/script1.js
@@ -5,5 +5,18 @@ Expressions_Assignment
  */
 
 //alert("hello world... This is in the JS file")
+//great for checking values and debugging
+//also shows all errors
+//console.log("this is the console.1")
+
+// prompt the user for width and height
+//explains to them whats going on
+//var height = prompt("let calculate the are of a rectangle \nPlease enter the height enter the height");
+//var width = prompt("Please enter the with of the  rectangle:");
+//var total= height*width
+//console.log(total)
 
 
+alert("Hello Click \"ok\" to continue Stress Level Calculator");//alert command tell user about the project
+var textString = ["0On a scale from 1-10, \n How much stress do you have","1On a scale from 1-10, \n How RELAX are you right now at work?","2How many REPORTS do you have to complete?","3Do you feel that violence is the ONLY solution?"]
+var outPutString = ["0Your STRESS level is ",'1Your RELAX level is ',"2The number of reports until complete is ","3You feel that violence is the ONly solutoin in the work place"]
